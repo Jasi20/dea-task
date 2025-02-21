88bb254b38b1e514054741b758ebe86a8baf4d7a
Jasi20 <davudceferov6600@gmail.com>
2025-02-21 20:16:57 +0400

    duzelis

--- ededlerinortasi/ders9.js
+++ ededlerinortasi/ders9.js
@@ -1,3 +1,3 @@
 let prices = [15, 20, 35, 40, 50, 60];
-let arr = Math.round((15 + 20 + 35 + 40 + 50 + 60) / prices.length)
+let arr = Math.round((prices[0] + prices[1] + prices[2] + prices[3] + prices[4] + prices[5]) / prices.length)
 console.log(arr);
